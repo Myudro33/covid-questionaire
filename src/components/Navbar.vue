bcb28e79e12f09cd95eb3f9f607741a671cdcd62
Myudro33 <nika.kanashvili@redberry.ge>
2023-05-12 14:13:53 +0400

    feat: create dynamic navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex flex-col mt-24">
+    <div class="flex justify-between items-center">
+      <img src="../assets/redberry-logo.svg" alt="logo" />
+      <h1 class="text-[40px] tracking-widest text-[#232323]">{{ `${page}/4` }}</h1>
+    </div>
+    <hr class="border-[1.8px] border-[#232323] mt-6" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["page"],
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import './assets/main.css'
-
+import Navbar from './components/Navbar.vue'
+import Input from './components/Input.vue'
+import Label from './components/Label.vue'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -7,5 +9,8 @@ import router from './router'
 const app = createApp(App)
 
 app.use(router)
+app.component('navbar',Navbar)
+app.component('input-component',Input)
+app.component('label-component',Label)
 
 app.mount('#app')
