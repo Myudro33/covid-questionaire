f6d89cbc43854bfce2b80070961246d3295adce6
Myudro33 <nika.kanashvili@redberry.ge>
2023-05-12 14:13:31 +0400

    feat: create dynamic label component

---
+++ src/components/Label.vue
@@ -0,0 +1,11 @@
+<template>
+  <label class="text-[22px] text-[#232323] font-bold mb-3 mt-[47px]" :for="label">{{
+    title
+  }}</label>
+</template>
+
+<script>
+export default {
+  props: ["label", "title"],
+};
+</script>
