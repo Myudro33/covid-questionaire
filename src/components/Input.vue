ac73cb80b8c3bf1fef098bc5e11760e192a8069f
Myudro33 <nika.kanashvili@redberry.ge>
2023-05-12 14:13:14 +0400

    feat: create dynamic input component

---
+++ src/components/Input.vue
@@ -0,0 +1,15 @@
+<template>
+  <input
+    :id="id"
+    :v-model="model"
+    class="w-[513px] h-[50px] px-5 border border-[#232323] text-lg"
+    type="text"
+    :placeholder="placeholder"
+  />
+</template>
+
+<script>
+export default {
+  props: ["id", "model", "placeholder"],
+};
+</script>
