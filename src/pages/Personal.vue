8a3d6c32aa9e4233b405112e4bca0bbbb890f14e
Myudro33 <nika.kanashvili@redberry.ge>
2023-05-15 13:58:18 +0400

    refactor: personal info page input validations

--- src/pages/Personal.vue
+++ src/pages/Personal.vue
@@ -2,16 +2,31 @@
   <div class="px-[200px]">
     <navbar :page="1" />
     <div class="w-full h-auto flex justify-between">
-      <Form @submit="handleSubmit" :validation-schema="schema" class="flex flex-col">
-        <input-component v-model="data.firstName" id="firstName" label="სახელი*" />
-        <input-component v-model="data.lastName" id="lastName" label="გვარი*" />
-        <input-component v-model="data.email" id="email" label="მეილი*" />
+      <Form @submit="onSubmit" v-slot="{ meta }" class="flex flex-col">
+        <input-component
+          :rules="'required|min:3|max:255'"
+          v-model="data.firstName"
+          id="firstName"
+          label="სახელი*"
+        />
+        <input-component
+          :rules="'required|min:3|max:255'"
+          v-model="data.lastName"
+          id="lastName"
+          label="გვარი*"
+        />
+        <input-component
+          :rules="'required|email|redberry_email'"
+          v-model="data.email"
+          id="email"
+          label="მეილი*"
+        />
         <div class="w-[270px] flex flex-col mt-28">
           <hr class="border-[#000000] mb-5 w-11/12" />
           <p class="text-[#626262]">*-ით მონიშნული ველების შევსება სავალდებულოა</p>
         </div>
         <div class="absolute bottom-12 left-1/2">
-          <button type="submit">
+          <button :disabled="!meta.valid" type="submit">
             <img src="../assets/vector.svg" alt="" />
           </button>
         </div>
@@ -23,41 +38,20 @@
 
 <script>
 import { Form, Field, ErrorMessage } from "vee-validate";
-import * as yup from "yup";
 import PersonalPageAnimation from "../components/PersonalPageAnimation.vue";
 export default {
   data() {
-    const schema = yup.object({
-      firstName: yup
-        .string()
-        .required("ეს ველი სავალდებულოა")
-        .min(3, "მინიმუმ 3 სიმბოლო")
-        .max(255, "სახელის ველი უნდა შედგებოდეს მაქსიმუმ 255 სიმბოლოსგან"),
-      lastName: yup
-        .string()
-        .required("ეს ველი სავალდებულოა")
-        .min(3, "მინიმუმ 3 სიმბოლო")
-        .max(255, "გვარის ველი უნდა შედგებოდეს მაქსიმუმ 255 სიმბოლოსგან"),
-      email: yup
-        .string()
-        .matches(
-          /.*\@redberry.ge$/,
-          "გთხოვთ დარეგისტრირდეთ Redberry-ს მეილით (youremail@redberry.ge)"
-        )
-        .required("ეს ველი სავალდებულოა"),
-    });
     const data = {
       firstName: localStorage.getItem("firstName") || "",
       lastName: localStorage.getItem("lastName") || "",
       email: localStorage.getItem("email") || "",
     };
     return {
-      schema,
       data,
     };
   },
   methods: {
-    handleSubmit() {
+    onSubmit() {
       this.$store.dispatch("personal/storePersonalData", this.data);
       this.$router.push({ path: "/covid-questions" });
     },
