5e7ec4f6a7cbf5120f9bae23cad9a50e1d781881
Myudro33 <nika.kanashvili@redberry.ge>
2023-05-16 00:32:58 +0400

    style: create vaccinated page animation

---
+++ src/components/VaccinationPageAnimation.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="relative translate-y-10">
+    <img class="z-20 relative" src="../assets/vaccinated.svg" alt="" />
+    <transition appear>
+      <img
+        class="w-[288px] h-[312px] absolute left-[30px] top-[-30px]"
+        src="../assets/vaccinated-anim.svg"
+        alt=""
+      />
+    </transition>
+  </div>
+</template>
+
+<style scoped>
+.v-enter-from {
+  left: 0px;
+  top: 300px;
+  width: 150px;
+  height: 150px;
+}
+.v-enter-active {
+  transition: all 0.3s ease-in;
+}
+.v-enter-to {
+  opacity: 0.8;
+}
+</style>
