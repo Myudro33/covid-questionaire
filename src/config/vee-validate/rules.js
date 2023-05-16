18fcda16faf2f6d008f67eb1e8c2e854052298d1
Myudro33 <nika.kanashvili@redberry.ge>
2023-05-16 18:52:19 +0400

    fix: redberry email validation rule

--- src/config/vee-validate/rules.js
+++ src/config/vee-validate/rules.js
@@ -7,9 +7,6 @@ defineRule("min", min);
 defineRule("max", max);
 
 defineRule("redberry_email", (value) => {
-    if (!value || !value.length) {
-      return true;
-    }
     if (!/^[a-z][a-z0-9_.]*@redberry.ge+$/.test(value)) {
       return false;
     }
