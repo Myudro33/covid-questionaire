448dc9f81da145928a71b5dd15f0e1a0257b36c5
Myudro33 <nika.kanashvili@redberry.ge>
2023-05-19 12:58:01 +0400

    style: fix submit button

--- src/pages/CovidQuestions.vue
+++ src/pages/CovidQuestions.vue
@@ -22,7 +22,10 @@
         </div>
         <ErrorMessage class="text-[#F15524] my-4" :name="'had_covid'" />
         <div v-if="covid === 'yes'" class="mt-11">
-          <label-component :label="'antibody'" :title="'ანტისხეულების ტესტი გაქვს გაკეთებული?*'" />
+          <label-component
+            :label="'antibody'"
+            :title="'ანტისხეულების ტესტი გაქვს გაკეთებული?*'"
+          />
           <div>
             <radio-input
               :rules="'required'"
@@ -31,11 +34,19 @@
               labelValue="კი"
               value="yes"
             />
-            <radio-input v-model="antibody_test" id="antibody_test" labelValue="არა" value="no" />
+            <radio-input
+              v-model="antibody_test"
+              id="antibody_test"
+              labelValue="არა"
+              value="no"
+            />
           </div>
         </div>
         <ErrorMessage class="text-[#F15524] my-4" :name="'antibody_test'" />
-        <div v-if="antibody_test === 'no' && covid === 'yes'" class="mt-11 h-22 flex flex-col">
+        <div
+          v-if="antibody_test === 'no' && covid === 'yes'"
+          class="mt-11 h-22 flex flex-col"
+        >
           <label-component
             :label="'antibody'"
             :title="'მიუთითე მიახლოებითი პერიოდი (დღე/თვე/წელი) როდის გქონდა Covid-19*'"
@@ -49,7 +60,10 @@
             />
           </div>
         </div>
-        <div v-if="antibody_test === 'yes' && covid === 'yes'" class="mt-11 flex flex-col">
+        <div
+          v-if="antibody_test === 'yes' && covid === 'yes'"
+          class="mt-11 flex flex-col"
+        >
           <label-component
             :label="'antibody'"
             :title="'თუ გახსოვს, გთხოვ მიუთითე ტესტის მიახლოებითი რიცხვი და ანტისხეულების რაოდენობა*'"
@@ -68,13 +82,18 @@
             />
           </div>
         </div>
-        <div class="absolute bottom-12 left-1/2 w-28 flex justify-between">
+        <div class="absolute bottom-12 left-1/2 w-28 flex justify-between z-50">
           <button type="button" @click="redirectBack">
             <img class="rotate-180" src="../assets/vector.svg" alt="" />
           </button>
           <button :disabled="!meta.valid" type="submit">
             <img v-if="meta.valid" src="../assets/vector.svg" alt="" />
-            <img v-else class="cursor-not-allowed" src="../assets/vector-gray.svg" alt="" />
+            <img
+              v-else
+              class="cursor-not-allowed"
+              src="../assets/vector-gray.svg"
+              alt=""
+            />
           </button>
         </div>
       </Form>
@@ -84,44 +103,44 @@
 </template>
 
 <script>
-import { Form, ErrorMessage } from 'vee-validate'
-import CovidPageAnimation from '../components/CovidPageAnimation.vue'
+import { Form, ErrorMessage } from "vee-validate";
+import CovidPageAnimation from "../components/CovidPageAnimation.vue";
 export default {
   components: { Form, ErrorMessage, CovidPageAnimation },
   data() {
     return {
-      covid: localStorage.getItem('had_covid') || null,
-      antibody_test: localStorage.getItem('antibody_test') || null,
-      date: localStorage.getItem('had_covid_date') || null,
-      test_number: localStorage.getItem('test_number') || null,
-      antibody: localStorage.getItem('antibody') || null
-    }
+      covid: localStorage.getItem("had_covid") || null,
+      antibody_test: localStorage.getItem("antibody_test") || null,
+      date: localStorage.getItem("had_covid_date") || null,
+      test_number: localStorage.getItem("test_number") || null,
+      antibody: localStorage.getItem("antibody") || null,
+    };
   },
   watch: {
     covid: (value) => {
-      localStorage.setItem('had_covid', value)
+      localStorage.setItem("had_covid", value);
     },
     antibody_test: (value) => {
-      localStorage.setItem('antibody_test', value)
-    }
+      localStorage.setItem("antibody_test", value);
+    },
   },
   methods: {
     onSubmit() {
-      this.$router.push({ path: '/vaccination' })
-      this.$store.dispatch('covid_questions/hadCovid', this.covid)
-      if (this.covid === 'yes') {
-        this.$store.dispatch('covid_questions/hadAntibodyTest', this.antibody_test)
-        if (this.antibody_test === 'yes') {
-          this.$store.dispatch('covid_questions/antibodies', this.antibody)
-          this.$store.dispatch('covid_questions/testDate', this.test_number)
+      this.$router.push({ path: "/vaccination" });
+      this.$store.dispatch("covid_questions/hadCovid", this.covid);
+      if (this.covid === "yes") {
+        this.$store.dispatch("covid_questions/hadAntibodyTest", this.antibody_test);
+        if (this.antibody_test === "yes") {
+          this.$store.dispatch("covid_questions/antibodies", this.antibody);
+          this.$store.dispatch("covid_questions/testDate", this.test_number);
         } else {
-          this.$store.dispatch('covid_questions/sicknessDate', this.date)
+          this.$store.dispatch("covid_questions/sicknessDate", this.date);
         }
       }
     },
     redirectBack() {
-      this.$router.push({ path: '/personal' })
-    }
-  }
-}
+      this.$router.push({ path: "/personal" });
+    },
+  },
+};
 </script>
