315d36e16d8c03bf80dade58f936320bbc8f53e1
Myudro33 <nika.kanashvili@redberry.ge>
2023-05-19 12:59:31 +0400

    chore: code format

--- src/pages/Personal.vue
+++ src/pages/Personal.vue
@@ -28,12 +28,7 @@
         <div class="absolute bottom-12 left-1/2 z-50">
           <button :disabled="!meta.valid" type="submit">
             <img v-if="meta.valid" src="../assets/vector.svg" alt="" />
-            <img
-              v-else
-              class="cursor-not-allowed"
-              src="../assets/vector-gray.svg"
-              alt=""
-            />
+            <img v-else class="cursor-not-allowed" src="../assets/vector-gray.svg" alt="" />
           </button>
         </div>
       </Form>
@@ -43,25 +38,25 @@
 </template>
 
 <script>
-import { Form, Field, ErrorMessage } from "vee-validate";
-import PersonalPageAnimation from "../components/PersonalPageAnimation.vue";
+import { Form, Field, ErrorMessage } from 'vee-validate'
+import PersonalPageAnimation from '../components/PersonalPageAnimation.vue'
 export default {
   data() {
     return {
-      firstName: localStorage.getItem("firstName") || "",
-      lastName: localStorage.getItem("lastName") || "",
-      email: localStorage.getItem("email") || "",
-    };
+      firstName: localStorage.getItem('firstName') || '',
+      lastName: localStorage.getItem('lastName') || '',
+      email: localStorage.getItem('email') || ''
+    }
   },
   methods: {
     onSubmit() {
-      this.$store.dispatch("personal/firstName", this.firstName);
-      this.$store.dispatch("personal/lastName", this.lastName);
-      this.$store.dispatch("personal/email", this.email);
+      this.$store.dispatch('personal/firstName', this.firstName)
+      this.$store.dispatch('personal/lastName', this.lastName)
+      this.$store.dispatch('personal/email', this.email)
 
-      this.$router.push({ path: "/covid-questions" });
-    },
+      this.$router.push({ path: '/covid-questions' })
+    }
   },
-  components: { Form, PersonalPageAnimation, Field, ErrorMessage },
-};
+  components: { Form, PersonalPageAnimation, Field, ErrorMessage }
+}
 </script>
