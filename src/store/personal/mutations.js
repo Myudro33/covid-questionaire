1205b0c155ac47bf40e4f8020632f9794a109e24
Myudro33 <nika.kanashvili@redberry.ge>
2023-05-21 19:29:09 +0400

    fix: delete unused setlocalstorage

--- src/store/personal/mutations.js
+++ src/store/personal/mutations.js
@@ -1,14 +1,11 @@
 export default {
   firstName(state, payload) {
     state.first_name = payload
-    localStorage.setItem('firstName', payload)
   },
   lastName(state, payload) {
     state.last_name = payload
-    localStorage.setItem('lastName', payload)
   },
   email(state, payload) {
     state.email = payload
-    localStorage.setItem('email', payload)
   }
 }
