9010f352c20e88d06e3d517bf0bd2ec3bf75af01
Myudro33 <nika.kanashvili@redberry.ge>
2023-05-22 17:42:18 +0400

    style: fix ball animation from different pages routing

--- src/components/CovidPageAnimation.vue
+++ src/components/CovidPageAnimation.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="relative -translate-y-10">
     <img class="z-20 relative" src="../assets/covid.svg" alt="ill boy" />
-    <transition appear>
+    <transition name="circle" v-if="route === 'vaccination'" appear>
+      <img
+        class="h-[200px] absolute left-[105px] top-[260px]"
+        src="../assets/covid-anim.png"
+        alt="red circle"
+      />
+    </transition>
+    <transition v-else appear>
       <img
         class="h-[200px] absolute left-[105px] top-[260px]"
         src="../assets/covid-anim.png"
@@ -10,6 +17,24 @@
     </transition>
   </div>
 </template>
+
+<script>
+import { useRouter } from "vue-router";
+
+export default {
+  setup() {
+    const router = useRouter();
+    const route = localStorage.getItem("from");
+    router.beforeEach((_, from, next) => {
+      localStorage.setItem("from", from.name);
+      next();
+    });
+    return {
+      route,
+    };
+  },
+};
+</script>
 
 <style scoped>
 .v-enter-from {
@@ -21,5 +46,12 @@
 }
 .v-enter-to {
   width: 250px;
+}
+.circle-enter-from {
+  transform: translate(100px, -200px);
+  opacity: 0;
+}
+.circle-enter-active {
+  transition: all 0.3s ease-out;
 }
 </style>
